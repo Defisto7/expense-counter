fcc7bec8cc3b93057243f615215ee0de46cd3970
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-06-06 11:56:09 +0300

    master
    add chart

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 
@@ -17,7 +18,8 @@ function Expenses(props) {
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>  
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
