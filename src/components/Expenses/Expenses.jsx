8d6e697628ef159b0f4b2db41b149e02b8d364ee
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-06-06 10:43:35 +0300

    master
    refact code

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2022');
+  const [filteredYear, setFilteredYear] = useState('2021');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear)
   };
+    const filteredExpenses = props.items.filter(expense => {
+      return expense.date.getFullYear().toString() === filteredYear
+    });
 
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {props.items.map((el) => (
-          <ExpenseItem key={el.id} title={el.title} amount={el.amount} date={el.date} />
-        ))}
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>  
+      <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
